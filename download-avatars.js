5ef5b6e87d34f26fe4e9a1a940fd29c919eafff5
Ryan Kendrick <rpkendrick@gmail.com>
2018-10-29 23:13:15 +0000

    created secrets.js file with github token, getRepoContributors function parses JSON string into an object and passes object to cb function

--- download-avatars.js
+++ download-avatars.js
@@ -13,13 +13,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    var parse = JSON.parse(body);
+    cb(err, parse);
   });
 }
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
+   for (var i = 0; i < result.length; i++) {
+    console.log(result[i]["avatar_url"]);
+
+  };
   console.log("Errors:", err);
   console.log("Result:", result);
 });
-
