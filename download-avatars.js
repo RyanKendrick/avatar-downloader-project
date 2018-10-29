14ff47df790f3c7f5232171b4bb64eafc5f221b6
Ryan Kendrick <rpkendrick@gmail.com>
2018-10-29 23:25:08 +0000

    implemented downloadImageByUrl and tested with hardcoded parameters

--- download-avatars.js
+++ download-avatars.js
@@ -1,5 +1,8 @@
 var request = require('request');
 var token = require('./secrets');
+var fs = require('fs');
+var https = require('https');
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -13,12 +16,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
+    // parse the JSON string into an object and pass this object
+    //(an array of contributor objects) to the callback function.
     var parse = JSON.parse(body);
     cb(err, parse);
   });
 }
 
-
+// callback function
 getRepoContributors("jquery", "jquery", function(err, result) {
    for (var i = 0; i < result.length; i++) {
     console.log(result[i]["avatar_url"]);
@@ -27,3 +32,14 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+function downloadImageByURL(url, filePath) {
+  request(url).pipe(fs.createWriteStream(filePath))
+
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+
+
+
+
